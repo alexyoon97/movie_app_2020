446b4481b3120a60c276cc347cba24abebb8896d
Alex Yoon <wonsangalex@gmail.com>
2020-05-01 20:17:37 -0700

    Updates

---
+++ precache-manifest.492dce3641c09af1b0f55618a627d308.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4672e2d0d59d3086841ec716761dc4fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7153e343e0e15a42138",
+    "url": "/static/css/main.5f84b063.chunk.css"
+  },
+  {
+    "revision": "c72f9673a23c79cc6cec",
+    "url": "/static/js/2.bfe070a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bfe070a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7153e343e0e15a42138",
+    "url": "/static/js/main.b5b061c8.chunk.js"
+  },
+  {
+    "revision": "459a6262b9a6f053411f",
+    "url": "/static/js/runtime-main.2db5a455.js"
+  }
+]);
